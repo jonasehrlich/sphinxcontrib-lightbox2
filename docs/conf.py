57e6ed42166ec0863e888f1e301943fe04b232fb
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2024-05-04 15:38:51 +0200

    Add Sphinx documentation setup

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from sphinxcontrib.lightbox2 import __version__
+
+project = "sphinxcontrib-lightbox2"
+copyright = "2024, Jonas Ehrlich"
+author = "Jonas Ehrlich"
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser", "sphinxcontrib.plantuml"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+
+# -- Options for myst-parser -------------------------------------------------
+# https://myst-parser.readthedocs.io/en/latest/configuration.html
+
+myst_enable_extensions = ["colon_fence"]
---
+++ docs/index.md
@@ -0,0 +1,23 @@
+<!-- markdownlint-disable MD041 -->
+
+:::{include} ../README.md
+:::
+
+## Examples
+
+### Figure
+
+:::{figure} large-diagram.drawio.svg
+Diagram with multiple components
+:::
+
+### Image
+
+:::{image} large-diagram.drawio.svg
+:::
+
+### PlantUML diagram
+
+:::{uml} sequence-chart.puml
+:caption: Caption with **bold** and *italic*
+:::
